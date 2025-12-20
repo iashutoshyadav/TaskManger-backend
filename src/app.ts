8d495f814113da498ecc306b1138d9f3f3104a49
Ashutosh Yadav <ashutosh2213072@akgec.ac.in>
2025-12-20 19:16:10 +0530

    fix issue

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: env.clientUrl,
+    origin: env.clientUrl, // https://task-manger-kappa-orpin.vercel.app
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -25,14 +27,15 @@ app.use(cookieParser());
 if (env.nodeEnv !== "production") {
   app.use(morgan("dev"));
 }
+
 app.get("/health", (_req, res) => {
   res.status(200).json({ status: "OK" });
 });
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/tasks", taskRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
-
 app.use(errorMiddleware);
 
 export default app;
